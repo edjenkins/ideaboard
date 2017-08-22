eb45aef132c5977a8c26f4da9d3132959492df7d
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-08-22 17:33:18 +0100

    Updated update script

--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
-cd app
+#!/bin/bash
+
+# Move into app dir
+cd ~/sites/ideaboard/app
+
+# Run npm build process
 npm run build
+
+# Move back out of app dir
 cd ..
+
+# Remove existing docker containers
 sudo docker-compose rm -f
+
+# Initialise and up docker containers
 sudo docker-compose up -d
-sudo docker-compose logs -f
