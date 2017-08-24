c15f237fa3f2c6cf4132ce3922a67074aa927da5
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-08-24 10:26:16 +0100

    Added facebook auth

--- api/app/routes/auth.js
+++ api/app/routes/auth.js
@@ -85,4 +85,13 @@ module.exports = function (app, passport) {
       })
     }
   })
+
+  // FACEBOOK ROUTES
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+    }));
 }
---
+++ api/config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  'facebookAuth': {
+    'clientID': process.env.FB_CLIENT_ID,
+    'clientSecret': process.env.FB_CLIENT_SECRET,
+    'callbackURL': process.env.FB_CALLBACK_URL
+  }
+
+};
