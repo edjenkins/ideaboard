9780b0d34b65979ddc3a1d0f464ca9abc1b8cfb6
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-08-24 15:19:25 +0100

    Updated requested facebook fields

--- api/config/passport.js
+++ api/config/passport.js
@@ -61,7 +61,8 @@ module.exports = function (passport) {
     // pull in our app id and secret from our auth.js file
     clientID: configAuth.facebookAuth.clientID,
     clientSecret: configAuth.facebookAuth.clientSecret,
-    callbackURL: configAuth.facebookAuth.callbackURL
+    callbackURL: configAuth.facebookAuth.callbackURL,
+    profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
 
   },
 
@@ -92,7 +93,7 @@ module.exports = function (passport) {
             newUser.facebook.id = profile.id; // set the users facebook id                   
             newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
             newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-            // newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+            newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
             // save our user to the database
             newUser.save(function (err) {
