591ab2c7604cf032a670f7a048447688430644db
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-08-24 14:33:56 +0100

    Updated auth routing

--- api/app/routes/auth.js
+++ api/app/routes/auth.js
@@ -87,11 +87,11 @@ module.exports = function (app, passport) {
   })
 
   // FACEBOOK ROUTES
-  app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
-
   app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
       successRedirect: '/profile',
       failureRedirect: '/'
     }));
+
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 }
