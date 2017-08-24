8e6d04031ca92e4e384eb9964aaf3429d354a072
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-08-24 14:16:01 +0100

    Updated instance homepages

--- app/src/components/instances/master.vue
+++ app/src/components/instances/master.vue
@@ -8,7 +8,6 @@
               .example {{ examples[exampleIndex] }}
             h3 Start a new idea and build your community through discussion and debate.
     
-    //- featured-ideas
     splash-image(align="right" title="Collaborative Design" subtitle="IdeaBoard is all about bringing people together in an online space to make decisions around specific topics." action="Learn More" link="/learn" image="https://images.pexels.com/photos/6224/hands-people-woman-working.jpg?w=940&h=650&auto=compress&cs=tinysrgb")
     showcase
     splash-image(align="left" title="Ready when you are." subtitle="When you've had a good look around the site and you're ready to start your own idea just click the button below." action="Create your Idea" link="/create" image="https://images.pexels.com/photos/212286/pexels-photo-212286.jpeg?w=940&h=650&auto=compress&cs=tinysrgb")
