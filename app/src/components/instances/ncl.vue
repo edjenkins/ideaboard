4e1e6b8f8b8353faf8655b56d0569ce76b83da87
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-08-24 14:13:11 +0100

    Added ncl instance homepage

---
+++ app/src/components/instances/ncl.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  #home
+    .row-wrapper#welcome(v-bind:style="{ 'background-color': navColor }")
+      .row
+        .content-block
+          .content-block--body
+            h1 Newcastle University designs 
+              .example {{ examples[exampleIndex] }}
+            h3 Start a new idea and build your community through discussion and debate.
+    
+    featured-ideas
+    splash-image
+
+    site-footer
+</template>
+
+<script>
+import Home from '@/mixins/Home'
+
+export default {
+  name: 'ncl',
+  metaInfo: {
+    title: 'Newcastle University'
+  },
+  mixins: [Home]
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~stylus/home'
+</style>
