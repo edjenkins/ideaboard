62a63a3d179173a4c95f5a8bdffd6ce5e0e50d52
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-02-19 22:52:02 +0000

    Updated showcase

--- app/src/components/home/Showcase.vue
+++ app/src/components/home/Showcase.vue
@@ -6,7 +6,7 @@
         ul.showcase
           li(v-for="(showcase, index) in showcases" v-bind:key="index")
             a(v-bind:href="showcase.link")
-              .showcase--image(v-bind:style="{ 'background-image': `url(${ showcase.image })` }")
+              .showcase--image(v-bind:style="{ 'background-image': `url(${ showcase.image })`, 'background-color': showcase.color }")
               .showcase--title {{ showcase.title }}
               .showcase--description {{ showcase.description }}
           .clearfix
@@ -24,19 +24,22 @@ export default {
           title: 'WEA',
           description: 'Ideaboard proved to be a great way to connect with our adult learners.',
           link: 'https://wea.ideaboard.co.uk',
-          image: ''
+          image: '',
+          color: '#006172'
         },
         {
           title: 'CityLit',
           description: 'We collected ideas from our students for new courses using Ideaboard.',
           link: 'https://citylit.ideaboard.co.uk',
-          image: ''
+          image: '',
+          color: '#7e179f'
         },
         {
           title: 'Newcastle University',
           description: 'We used Ideaboard to dynamically organise study sessions for students.',
           link: 'https://ncl.ideaboard.co.uk',
-          image: ''
+          image: '',
+          color: '#003865'
         }
       ]
     }
@@ -74,6 +77,7 @@ export default {
           text-decoration none
           .showcase--image
             background-image()
+            gradient()
             height 160px
           .showcase--title
             reset()
