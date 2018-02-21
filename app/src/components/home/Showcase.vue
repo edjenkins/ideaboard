f8fc7577af7aaad3e29444f58b4407b1998e228f
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-02-21 13:28:52 +0000

    Updated showcase

--- app/src/components/home/Showcase.vue
+++ app/src/components/home/Showcase.vue
@@ -6,7 +6,7 @@
         ul.showcase
           li(v-for="(showcase, index) in showcases" v-bind:key="index")
             a(v-bind:href="showcase.link")
-              .showcase--image(v-bind:style="{ 'background-image': `url(${ showcase.image })`, 'background-color': showcase.color }")
+              .showcase--image(v-bind:style="{ 'background-image': `url(${ showcase.image })`, 'border-bottom-color': showcase.color }")
               .showcase--title {{ showcase.title }}
               .showcase--description {{ showcase.description }}
           .clearfix
@@ -24,21 +24,21 @@ export default {
           title: 'WEA',
           description: 'Ideaboard proved to be a great way to connect with our adult learners.',
           link: 'https://wea.ideaboard.co.uk',
-          image: '',
+          image: 'https://s3.amazonaws.com/xmovement/ideaboard/assets/stock/linkedin-sales-navigator-406827-unsplash.jpg',
           color: '#006172'
         },
         {
           title: 'CityLit',
           description: 'We collected ideas from our students for new courses using Ideaboard.',
           link: 'https://citylit.ideaboard.co.uk',
-          image: '',
+          image: 'https://s3.amazonaws.com/xmovement/ideaboard/assets/stock/rawpixel-com-196464-unsplash.jpg',
           color: '#7e179f'
         },
         {
           title: 'Newcastle University',
           description: 'We used Ideaboard to dynamically organise study sessions for students.',
           link: 'https://ncl.ideaboard.co.uk',
-          image: '',
+          image: 'https://s3.amazonaws.com/xmovement/ideaboard/assets/stock/becca-tapert-391599-unsplash.jpg',
           color: '#003865'
         }
       ]
@@ -77,7 +77,7 @@ export default {
           text-decoration none
           .showcase--image
             background-image()
-            gradient()
+            border-bottom white 3px solid            
             height 160px
           .showcase--title
             reset()
