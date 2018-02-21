2d8d09e8cea6287a6828c8f5e9f020359b5b92ea
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-02-21 17:04:08 +0000

    Updtes

--- api/app/utilities.js
+++ api/app/utilities.js
@@ -1,5 +1,5 @@
 const DEV_URL = 'http://localhost:8888'
-const PROD_URL = 'https://*ideaboard.co.uk'
+const PROD_URL = 'https://*.ideaboard.co.uk'
 
 module.exports = {
   redirectUri: function (instance) {
@@ -7,7 +7,7 @@ module.exports = {
       case 'default':
         return DEV_URL
       case 'master':
-        return PROD_URL.replace('*', '')
+        return PROD_URL.replace('*.', '')
       default:
         return PROD_URL.replace('*', instance)
     }
