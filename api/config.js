7da0b00051cab1dd313672b5e423fb7e7a07d841
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-03-06 15:55:10 +0000

    Updated config

--- api/config.js
+++ api/config.js
@@ -1,35 +1,35 @@
 module.exports = {
-  "domain": "ideaboard.co.uk",
-  "instances": {
-    "default": "master",
-    "master": {
-      "color": "#0099D0"
+  domain: "ideaboard.co.uk",
+  instances: {
+    default: "master",
+    master: {
+      color: "#0099D0"
     },
-    "citylit": {
-      "color": "#7e179f",
-      "subdomain": "citylit"
+    citylit: {
+      color: "#7e179f",
+      subdomain: "citylit"
     },
-    "wea": {
-      "color": "#006172",
-      "subdomain": "wea"
+    wea: {
+      color: "#006172",
+      subdomain: "wea"
     },
-    "ncl": {
-      "color": "#003865",
-      "subdomain": "ncl"
+    ncl: {
+      color: "#003865",
+      subdomain: "ncl"
     },
-    "openlab": {
-      "color": "#0099D0",
-      "subdomain": "openlab"
+    openlab: {
+      color: "#0099D0",
+      subdomain: "openlab"
     },
-    "myto": {
-      "color": "#7DCE82",
-      "subdomain": "myto",
-      "categories": [
-        { "name": "Engaging with research" },
-        { "name": "Gaining independence and taking control" },
-        { "name": "Participating in arts / sport / recreation / music" },
-        { "name": "Connecting with people like me" },
-        { "name": "Fertility and parenthood" }
+    myto: {
+      color: "#7DCE82",
+      subdomain: "myto",
+      categories: [
+        { name: "Engaging with research" },
+        { name: "Gaining independence and taking control" },
+        { name: "Participating in arts / sport / recreation / music" },
+        { name: "Connecting with people like me" },
+        { name: "Fertility and parenthood" }
       ]
     }
   }
