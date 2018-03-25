3c1b6b8e1b64c2a39703b899e3c3e30aa0b1e236
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-03-25 00:38:06 +0000

    Updates

--- api/app.js
+++ api/app.js
@@ -72,6 +72,7 @@ app.use(session({
 
 // required for passport
 app.use(passport.initialize())
+app.use(passport.session()) // persistent login sessions
 app.use(flash()) // use connect-flash for flash messages stored in session
 
 app.get('/',
