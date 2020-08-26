26a7520502161a95c1518982a215c8071e68a0ea
Dalya <dalya.ali@newcastle.ac.uk>
2020-08-26 11:39:53 +0100

    updated unsplash

--- api/app/routes/unsplash.js
+++ api/app/routes/unsplash.js
@@ -1,15 +1,17 @@
-var unsplash = require('unsplash-api')
-
-var clientId = process.env.UNSPLASH_CLIENT_ID
-unsplash.init(clientId)
+// var unsplash = require('unsplash-api')
+const axios = require('axios');
 
 module.exports = function (app, passport) {
   // Search unsplash
   app.get('/unsplash/search/:query',
     (req, res) => {
-      // query, categories, page, perPage, callback
-      unsplash.searchPhotos(req.params.query, undefined, undefined, 12, (error, photos, link) => {
-        res.json(photos)
-      })
+      axios.get('https://api.unsplash.com/search/photos', { params: { 'query': req.params.query, 'client_id': process.env.UNSPLASH_CLIENT_ID } })
+      .then(response => {
+          // Your code
+          if (response.data) res.send(response.data.results)
+          else res.json([])
+      }).catch(function (error) {
+        console.log(error);
+      });
     })
 }
